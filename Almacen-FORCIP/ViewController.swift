7f36a7a891354d0ccd526197ea04dc85e3d2e941
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2025-10-13 11:43:28 -0600

    Initial Commit

---
+++ Almacen-FORCIP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Almacen-FORCIP
+//
+//  Created by Miguel Mexicano Herrera on 13/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Almacen-FORCIPTests/Almacen_FORCIPTests.swift
@@ -0,0 +1,17 @@
+//
+//  Almacen_FORCIPTests.swift
+//  Almacen-FORCIPTests
+//
+//  Created by Miguel Mexicano Herrera on 13/10/25.
+//
+
+import Testing
+@testable import Almacen_FORCIP
+
+struct Almacen_FORCIPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
